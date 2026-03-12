7ec809ac68465b787cd4fff2620af25ab39a5d4e
Nash <67327530+nxsh-lol@users.noreply.github.com>
2026-03-12 12:48:28 -0500

    Add files via upload

--- assets/script/bootstrap.161abcf791f5bd41c81d4bd54d6b0759e0b00371.js
+++ assets/script/bootstrap.161abcf791f5bd41c81d4bd54d6b0759e0b00371.js
@@ -5,7 +5,7 @@ function decode(component) {
 	return decodeURIComponent(component);
 }
 const hostInfo = {
-	version: "928bbca",
+	version: "7113b64",
 	scopeCanvas: JSON.parse("true"),
 };
 const projectIdRegex = /^\/projects\/(?<prefix>(?:(?:[A-Za-z0-9]+-)*[A-Za-z0-9]+--)?)(?<id>[A-Za-z0-9]{20})(?<accessToken>(?:-[A-Za-z0-9]+)?)/;
